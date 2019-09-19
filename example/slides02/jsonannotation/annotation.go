35fbfee7ab258ec37103d389af950406ad9898c0
NikitaSmall <nikitasosnov@yahoo.com>
2019-09-19 10:36:07 +0300

    fix typo

--- example/slides02/jsonannotation/annotation.go
+++ example/slides02/jsonannotation/annotation.go
@@ -20,7 +20,7 @@ type User struct {
 	gender string // no marshal, as the field is private (isn't exported)
 }
 
-// NewUser is just a constuctor function
+// NewUser is just a constructor function
 // no sign of annotation here, it's just a tradition that only exported fields should be annotated
 func NewUser(name, profession, gender string, age int) *User {
 	return &User{
